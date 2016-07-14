c575f3266f08e19ee7f1157383df9a6862d77f0d
swyt <swytman@gmail.com>
2016-07-15 01:03:02 +0300

    change loader

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,15 +7,15 @@ set :pm2_app_command, 'npm start'
 set :repo_url, 'git@github.com:swytman/nhleditor.git'
 set :branch, "master"
 set :deploy_to, '/var/www/nhleditor'
-set :linked_dirs, %w{node_modules}
+set :linked_dirs, %w{node_modules data}
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    # invoke 'npm:install'
-    invoke 'pm2:delete'
-    invoke 'pm2:start'
-  end
-
-  after :publishing, :restart
-end
+# namespace :deploy do
+#   desc 'Restart application'
+#   task :restart do
+#     # invoke 'npm:install'
+#     invoke 'pm2:delete'
+#     invoke 'pm2:start'
+#   end
+#
+#   after :publishing, :restart
+# end
