b100a87b49ff55f0413292e0e08a86c18764de06
Stepanov Viacheslav <stepanov.vm@gmail.com>
2016-07-16 18:06:07 +0300

    change name

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 # config valid only for current version of Capistrano
 
 set :application, 'nhleditor'
-set :pm2_app_command, 'pm2 start npm --name "NhlEditor" -- start'
+set :pm2_app_command, 'pm2 start npm --name "nhleditor" -- start'
 set :repo_url, 'git@github.com:swytman/nhleditor.git'
 set :branch, "master"
 set :deploy_to, '/var/www/nhleditor'
